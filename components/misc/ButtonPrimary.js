758ccfccb2a8afceda2f4675ad0a519191cc6b77
akumar <akumar@comtronusa.com>
2023-07-07 05:27:46 -0400

    added files

---
+++ components/misc/ButtonPrimary.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ButtonPrimary = ({ children, addClass, link }) => {
+  const handleNavigate = () => {
+    console.log("clicked", link);
+    window.open(link, "_blank", "noopener,noreferrer");
+  };
+  return (
+    <button
+      onClick={handleNavigate}
+      className={
+        "py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-orange-500 hover:shadow-orange-md transition-all outline-none " +
+        addClass
+      }
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ButtonPrimary;
